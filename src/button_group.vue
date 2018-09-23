fdee56aa86ad00ea612b67929220ea68a5a45d08
barry.cao <1035102183@qq.com>
2018-09-23 16:28:51 +0800

    添加 button_group

---
+++ src/button_group.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="p-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script></script>
+<style lang="scss">
+    .p-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        > .p-button{
+            border-radius: 0;
+            margin-right: -1px;
+            &:first-child{
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover{
+                position: relative;z-index: 1;
+            }
+        }
+    }
+</style>
