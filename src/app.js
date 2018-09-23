77e2ca9f9a21bc403cb288e8e24b3bb47de32ee0
barry.cao <1035102183@qq.com>
2018-09-23 18:42:41 +0800

    添加四个测试用例，测试项分别为 icon，position，order，function

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ import Icon from './icon'
 import ButtonGroup from './button_group'
 
 Vue.component('p-button',Button);
-Vue.component('p-icon',Icon)
-Vue.component('p-button-group',ButtonGroup)
+Vue.component('p-icon',Icon);
+Vue.component('p-button-group',ButtonGroup);
 
+// noinspection ObjectAllocationIgnored
 new Vue({
     el: '#app',
     data:{
@@ -14,3 +15,85 @@ new Vue({
         loading2: false
     }
 });
+import chai from 'chai'
+const expect = chai.expect;
+// 单元测试
+{
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting'
+        }
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let herf = useElement.getAttribute('xlink:href');
+    expect(herf).to.eq('#i-setting');
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading: true
+        }
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let herf = useElement.getAttribute('xlink:href');
+    expect(herf).to.eq('#i-loading');
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading: true
+        }
+    });
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq('1');
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+            iconPosition: 'right'
+        }
+    });
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq('2');
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    //mock
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+        }
+    });
+    vm.$mount();
+    vm.$on('click',function () {
+        console.log('1');
+    });
+    //希望函数被执行
+    let button = vm.$el;
+    button.click();
+}
