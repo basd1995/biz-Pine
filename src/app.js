42901eb7da511ab5929decdd5841529a1455fca2
barry.cao <barry.cao@bizfocus.cn>
2019-04-09 14:53:28 +0800

    row col组件雏形

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button_group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('p-button', Button);
 Vue.component('p-icon', Icon);
 Vue.component('p-button-group', ButtonGroup);
 Vue.component('p-input', Input);
+Vue.component('p-row', Row);
+Vue.component('p-col', Col);
 
 // noinspection ObjectAllocationIgnored
 new Vue({
---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'PineCol',
+    props: {
+      span: {
+        type: [Number,String]
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+  .col{
+    height: 80px;
+    width: 50%;
+    background-color: gray;
+    border: 1px solid red;
+    
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24)*100%;
+      }
+    }
+  }
+</style>
